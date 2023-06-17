89d6d18f89a3c5881306e247d61f8cca75a8128f
funkyenough <funky.enough.jp@gmail.com>
2023-06-17 17:27:10 +0900

    change api to nextjs implementation

--- pages/api/Zotero.ts
+++ pages/api/Zotero.ts
@@ -1,28 +1,38 @@
-require("dotenv").config({ path: "./.env.local" });
+import { NextApiRequest, NextApiResponse } from "next";
+import fetch from "node-fetch";
 
-const fetchZoteroItems = async () => {
+export default async function fetchZoteroItems(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  require("dotenv").config({ path: "./.env.local" });
+
   const apiKey = process.env.Zotero_API_Key || "";
+  const userId = process.env.zoteroUserId || "";
+  console.log(apiKey, userId);
 
   const myHeaders = new Headers({
     "Zotero-API-Key": apiKey,
   });
+  console.log(myHeaders);
 
-  const url = `https://api.zotero.org/users/${process.env.zoteroUserId}/items`;
+  const url = `https://api.zotero.org/users/${userId}/items`;
 
-  try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    });
+  // try {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  });
 
-    const result = await response.json();
-    const titles = result.map((item) => item.data.title);
-    const dateAdded = result.map((item) => item.data.dateAdded);
-    console.log(titles, dateAdded);
-  } catch (error) {
-    console.log("Error:", error);
-  }
-};
+  const result = await response.json();
+  const titles = result.map((item) => item.data.title);
+  const dateAdded = result.map((item) => item.data.dateAdded);
 
-export default fetchZoteroItems();
+  res.status(200).json({ titles, dateAdded });
+  // } catch (error) {
+  //   res
+  //     .status(500)
+  //     .json({ error: "Something went wrong", message: error.message });
+  // }
+}

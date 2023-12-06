8f1ea67102957002c5baa275df7f6d30ee3d23a3
funkyenough <funky.enough.jp@gmail.com>
2023-12-06 16:13:28 +0900

    clean up styling for intro and projects component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,23 +3,26 @@ import ZoteroReadingList from "./ZoteroReadingList";
 function Intro() {
   return (
     <div className="flex flex-row text-center justify-center">
-      <div className="space-y-3">
-        <div>Flaneur, Generalist</div>
-        <div>I am a Philosophy/Psychology turned Software Developer.</div>
-        <div className="text-lg font-bold">Skills</div>
+      <div>
+        <div>
+          Flaneur, Generalist <br /> Philosopher turned Software Developer.
+        </div>
+        <div className="text-lg font-bold my-8">Skills</div>
         <div className="flex flex-row justify-around">
           <div className="flex flex-col">
-            <div>Web3</div>
+            <div className="font-bold">Web3</div>
             <div className="flex flex-col">
-              <div className="">
-                <div>Frontend</div>
-                <div>Backend</div>
-              </div>
-              <div>WAGMI, Viem, EthersJS</div>
+              <div>EthersJS</div>
               <div>Hardhat, Foundry</div>
             </div>
           </div>
-          <div>Web2</div>
+          <div className="flex flex-col">
+            <div className="font-bold">Web2</div>
+            <div className="flex flex-col">
+              <div>NextJS13</div>
+              <div>TailwindCSS</div>
+            </div>
+          </div>
         </div>
         <ZoteroReadingList />
         <div className="bg-black"></div>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,23 +5,24 @@ function Projects() {
   return (
     <div className="text-center">
       <div>
-        <div className="text-lg font-bold">Current Projects</div>
+        <div className="text-lg font-bold my-8">Current Projects</div>
         <div className="flex flex-col justify-center item-center">
-          <div>Philosophy Tools</div>
-          <div>Concept Expansion on Cartesian Plane</div>
+          <div>(Concept Expansion on Cartesian Plane)</div>
           {/* <CartesianPlane /> */}
         </div>
       </div>
       <div>
-        <div className="text-lg font-bold">Hackathon Projects</div>
+        <div className="text-lg font-bold my-8">Hackathon Projects</div>
         <div className="flex flex-row items-top">
           <div className="flex flex-col">
             <div className="font-bold">Harberger Nouns</div>
-            <div>NounsDAO fork with a Harberger Tax Implementation</div>
+            <div className="mx-5">
+              NounsDAO fork with a Harberger Tax Implementation
+            </div>
           </div>
           <div className="flex flex-col">
             <div className="font-bold">Noir-Battle</div>
-            <div>
+            <div className="mx-5">
               Implementation of Zupass' zk-eddsa-frog-pcd with Noir, a ZKDSL
             </div>
           </div>

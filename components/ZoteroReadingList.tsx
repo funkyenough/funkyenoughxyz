6a217de33936ff9f3e74002ba34aaac53a8681f8
funkyenough <funky.enough.jp@gmail.com>
2024-02-07 15:01:01 +0900

    Adding skeleton to zotero items

--- components/ZoteroReadingList.tsx
+++ components/ZoteroReadingList.tsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { formatDate } from "../utils/FormatDate";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 interface Item {
   title: string;
@@ -9,80 +12,65 @@ interface Item {
   url: string;
 }
 
-function SkeletonItem() {
-  return (
-    <li className="animate-pulse">
-      <div className="flex flex-row justify-between">
-        <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-        <div className="h-4 bg-gray-300 rounded w-1/4"></div>
-      </div>
-      <div className="h-4 bg-gray-300 rounded w-full mt-2"></div>
-    </li>
-  );
-}
-
-function ZoteroReadingList() {
-  // const [isLoading, setIsLoading] = useState(true);
+export default function ZoteroReadingList() {
   const [data, setData] = useState<Item[]>([]);
 
-  // if (isLoading) {
-  //   return (
-  //     <div>
-  //       <div className="text-lg font-bold">What I am reading lately</div>
-  //       <ul className="space-y-2">
-  //         {Array.from({ length: 5 }).map((_, index) => (
-  //           <SkeletonItem key={index} />
-  //         ))}
-  //       </ul>
-  //     </div>
-  //   );
-  // }
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/zotero");
+      let data: Item[] = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
 
   useEffect(() => {
-    fetch("/api/zotero")
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error("Error:", error));
-    // .finally(() => {
-    //   setIsLoading(false);
-    // });
+    fetchData();
   }, []);
-
-  // Function to convert ISO date to custom format (YYYY/MM/DD)
-  const formatDate = (isoDate: string | number | Date) => {
-    const dateObj = new Date(isoDate);
-    const year = dateObj.getFullYear();
-    const month = String(dateObj.getMonth() + 1).padStart(2, "0");
-    const day = String(dateObj.getDate()).padStart(2, "0");
-    return `${year}/${month}/${day}`;
-  };
 
   return (
     <div>
       <div className="text-lg font-bold my-8">What I am reading lately</div>
       <ul className="space-y-2">
-        {data.map((item) => (
-          <li key={item.title}>
-            <div className="flex flex-col">
+        {data.length === 0 ? (
+          <li>
+            <div className="flex flex-col border-2">
               <div className="flex flex-row justify-between">
                 <div>
-                  {item.firstName} {item.lastName}
+                  <Skeleton height={20} width={200} />
                 </div>
                 <div>
-                  <div>{formatDate(item.dateAdded)}</div>
+                  <Skeleton height={20} width={200} />
                 </div>
               </div>
-              <a href={item.url} className="text-left">
-                {item.title}
+              <a href="#" className="text-left">
+                <Skeleton height={20} width={200} />
               </a>
             </div>
-
-            <br />
           </li>
-        ))}
+        ) : (
+          data.map((item) => (
+            <li key={item.title}>
+              <div className="flex flex-col border-2">
+                <div className="flex flex-row justify-between">
+                  <div>
+                    {item.firstName} {item.lastName}
+                  </div>
+                  <div>
+                    <div>{formatDate(item.dateAdded)}</div>
+                  </div>
+                </div>
+                <a href={item.url} className="text-left">
+                  {item.title || <Skeleton />}
+                </a>
+              </div>
+
+              <br />
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
 }
-
-export default ZoteroReadingList;

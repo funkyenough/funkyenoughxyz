0bbae4c51f8af9ad3e9f9c0720daa5b9538568ef
funkyenough <14842981+funkyenough@users.noreply.github.com>
2024-02-21 17:11:46 +0900

    Delete utils/FormatDate.ts

--- utils/FormatDate.ts
+++
@@ -1,8 +0,0 @@
-// Function to convert ISO date to custom format  (YYYY/MM/DD)
-export const formatDate = (isoDate: string | number | Date) => {
-  const dateObj = new Date(isoDate);
-  const year = dateObj.getFullYear();
-  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
-  const day = String(dateObj.getDate()).padStart(2, "0");
-  return `${year}/${month}/${day}`;
-};

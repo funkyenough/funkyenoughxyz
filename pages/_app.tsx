2cb27d36789272d05ca0dc7eddea4f64a7df3f0f
funkyenough <funky.enough.jp@gmail.com>
2024-02-25 10:13:40 +0900

    Fix Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,12 @@ import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return;
-  <>
-    <Component {...pageProps} />
-    <Analytics />
-  </>;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;

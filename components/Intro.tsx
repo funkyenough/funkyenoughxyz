3695431836d47318c37f28b13afeba7a4e0f1e89
funkyenough <funky.enough.jp@gmail.com>
2024-02-26 14:03:51 +0900

    Revert Skills Component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,4 +1,4 @@
-import Skills from "./Skills";
+// import Skills from "./Skills";
 
 function Intro() {
   return (
@@ -8,8 +8,8 @@ function Intro() {
           Flaneur, Generalist <br /> Philosopher turned Software Developer.
         </div>
         <div className="my-8 text-lg font-bold">Skills</div>
-        <Skills />
-        {/* <div className="flex flex-row justify-around">
+        {/* <Skills /> */}
+        <div className="flex flex-row justify-around">
           <div className="flex flex-col">
             <div className="font-bold">Web3</div>
             <div className="flex flex-col">
@@ -24,7 +24,7 @@ function Intro() {
               <div>TailwindCSS</div>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
